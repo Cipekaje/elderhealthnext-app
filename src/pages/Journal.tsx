c1448ea46c32c20e5edac3178536f1dc8ea85204
Mindaugas <mindaugas@gmail.com>
2024-04-17 00:12:24 +0300

    Added authorization for pages

--- src/pages/Journal.tsx
+++ src/pages/Journal.tsx
@@ -1,9 +1,28 @@
 import UserJournalLayout from '@/layouts/UserJournalLayout';
+import { GetSessionParams, getSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
-export default function LoginPage() {
+export default function JournalPage() {
   return (
     <div className="container" style={{ maxHeight: '100vh', overflow: 'auto'}}>
       <UserJournalLayout />
     </div>
   );
+}
+//ar naudotojas prisijunges
+export async function getServerSideProps(context: GetSessionParams | undefined) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {}, // Pass any additional props here
+  };
 }
